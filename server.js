b0c666db4239d214d61b03151de621b964ce5a81
Tkachenko <tkachenko@a55-4.mydomain.com>
2015-05-18 16:10:57 +0300

    cors

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ var routes = require('./backend/routes');
 
 var server = express();
 
-server.use(cors({
-  allowedOrigins: [
-    '*'
-  ]
-}));
+// server.use(cors({
+//   allowedOrigins: [
+//     '127.0.0.1:3000',
+//     'localhost:3000',
+//     '52.10.64.218'
+//   ]
+// }));
+
+server.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 server.get('/', function(req, res) {
   res.end("it's an api server");
