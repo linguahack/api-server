f7fa52df87d376eed8a91469d52a9d4c9b4435ce
Oleksiy Tkachenko <olexiy.tkachenko@gmail.com>
2015-06-15 16:44:16 +0300

    delete jsonify

--- utils/jsonify.js
+++
@@ -1,9 +0,0 @@
-
-
-var jsonify_regex = /([\{,]\s*)(.+?)(\s*:)/gm;
-
-var jsonify = function(str) {
-  return str.replace(this.jsonify_regex, '$1"$2"$3').replace(/(:\s*)'([^']*)'(\s*[,\}])/g, '$1"$2"$3');
-};
-
-module.exports = jsonify;
